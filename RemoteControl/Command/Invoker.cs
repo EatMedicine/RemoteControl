4316f79b07c3be47639c9b01ad377524e2520a61
EatMedicine <870830676@qq.com>
2019-06-20 01:41:20 +0800

    起了个底稿

---
+++ RemoteControl/Command/Invoker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteControl.Command
+{
+    public class Invoker
+    {
+        private List<Command> cmdList;
+
+        public Invoker()
+        {
+            cmdList = new List<Command>();
+        }
+
+        public void AddCommand(Command cmd)
+        {
+            cmdList.Add(cmd);
+        }
+
+        public void ExecuteAll()
+        {
+            foreach(Command cmd in cmdList)
+            {
+                cmd.Execute();
+            }
+        }
+    }
+}
--- RemoteControl/Form1.Designer.cs
+++ RemoteControl/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(610, 339);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
